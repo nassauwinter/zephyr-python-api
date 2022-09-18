541f848a6b51439aaa410165d572d165ad613161
Petr Sharapenko <nassauwinter@gmail.com>
2022-09-18 19:45:38 +0300

    Add Zephyr Sscale server endpoints for Test Cases

--- zephyr/scale/server/enpoints/test_cases.py
+++ zephyr/scale/server/enpoints/test_cases.py
@@ -7,33 +7,39 @@ class TestCaseEndpoints:
         self.session = session
 
     def create_test_case(self, project_key, name, **kwargs):
-        pass
+        json = {"projectKey": project_key,
+                "name": name}
+        json.update(kwargs)
+        return self.session.post("testcase", json=json)
 
-    def get_test_case(self, test_case_key, **kwargs):
-        return self.session.get(f"testcase/{test_case_key}", params=kwargs)
+    def get_test_case(self, test_case_key, **params):
+        return self.session.get(f"testcase/{test_case_key}", params=params)
 
-    def get_all_versions(self, fields="id,majorVersion"):
-        return self.session.get(f"TRT-T26244/allVersions", fields=fields)
+    def update_test_case(self, test_case_key, **json):
+        return self.session.put(f"testcase/{test_case_key}", json=json)
 
-    def update_test_case(self, test_case_key, **kwargs):
-        pass
+    def delete_test_case(self, test_case_key):
+        return self.session.delete(f"testcase/{test_case_key}")
 
     def get_attachments(self, test_case_key):
-        pass
+        return self.session.get(f"testcase/{test_case_key}/attachments")
 
-    def create_attachment(self, test_case_key, file):
-        # multipart/form-data content type
-        pass
+    def create_attachment(self, test_case_key, file_path):
+        with open(file_path, "rb") as file:
+            files = {"file": file}
+            return self.session.post(f"testcase/{test_case_key}/attachments", files=files)
 
     def get_latest_result(self, test_case_key):
-        pass
+        return self.session.get(f"testcase/{test_case_key}/testresult/latest")
 
-    def get_step_attachments(self, step_index, test_case_key):
-        pass
+    def get_step_attachments(self, test_case_key, step_index):
+        return self.session.get(f"testcase/{test_case_key}/step/{step_index}/attachments")
 
-    def create_step_attachment(self, step_index, test_case_key, file):
-        # multipart/form-data content type
-        pass
+    def create_step_attachment(self, test_case_key, step_index, file_path):
+        with open(file_path, "rb") as file:
+            files = {"file": file}
+            return self.session.post(f"testcase/{test_case_key}/step/{step_index}/attachments", files=files)
 
-    def search_cases(self, **kwargs):
-        pass
+    def search_cases(self, query, **params):
+        params.update({"query": query})
+        return self.session.get("testcase/search", params=params)
