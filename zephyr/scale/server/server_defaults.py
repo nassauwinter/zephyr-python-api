d0c4f310ab41e4a31e674cbbf57ce366fe6ef1e6
PetrS <nassauwinter@gmail.com>
2022-09-18 20:44:43 +0200

    Server API: Update defaults with test case defaults

--- zephyr/scale/server/server_defaults.py
+++ zephyr/scale/server/server_defaults.py
@@ -4,4 +4,15 @@ This module contains default values for various Server API entities
 
 
 class TestCaseDefaults:
+    CASE_FIELDS = ("id,projectId,archived,key,name,objective,majorVersion,latestVersion,"
+                   "precondition,folder(id,fullName),status,priority,estimatedTime,"
+                   "averageTime,componentId,owner,labels,customFieldValues,testScript(id,text,"
+                   "steps(index,description,text,expectedResult,testData,attachments,"
+                   "customFieldValues,id,stepParameters(id,testCaseParameterId,value),testCase("
+                   "id,key,name,archived,majorVersion,latestVersion,parameters(id,name,"
+                   "defaultValue,index)))),testData,parameters(id,name,defaultValue,index),"
+                   "paramType")
+    TEST_RESULT_FIELDS = ("testResultStatus(name,i18nKey,color),environment(name),key,userKey,"
+                          "assignedTo,jiraVersionId,estimatedTime,executionTime,executionDate,"
+                          "automated,testRun,testCase,issueLinks,sprint(name)")
     VERSION_FIELDS = "id,majorVersion"
