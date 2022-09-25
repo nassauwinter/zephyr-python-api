4f58926862b18006ece68845299722a71ac3e6c6
PetrS <nassauwinter@gmail.com>
2022-09-25 21:07:29 +0200

    SERVER API: Add wrappers for test plan and test run endpoints

--- zephyr/scale/server/enpoints/endpoints.py
+++ zephyr/scale/server/enpoints/endpoints.py
@@ -51,3 +51,78 @@ class TestCaseEndpoints(EndpointTemplate):
     def get_all_versions(self, test_case_key, **params):
         """Get all test case versions ids by its key name. Undocumented in API"""
         return self.session.get(Paths.CASE_VERS.format(test_case_key), params=params)
+
+
+class TestPlanEndpoints(EndpointTemplate):
+    """Api wrapper for "Test Plan" endpoints"""
+
+    def create_test_plan(self, project_key, name, **kwargs):
+        json = {"projectKey": project_key,
+                "name": name}
+        json.update(kwargs)
+        return self.session.post(Paths.PLAN, json=json)
+
+    def get_test_plan(self, test_plan_key, **params):
+        return self.session.get(Paths.PLAN_KEY.format(test_plan_key),
+                                params=params)
+
+    def update_test_plan(self, test_plan_key, **json):
+        return self.session.put(Paths.PLAN_KEY.format(test_plan_key),
+                                json=json)
+
+    def delete_test_plan(self, test_plan_key):
+        return self.session.delete(Paths.PLAN_KEY.format(test_plan_key))
+
+    def get_attachments(self, test_plan_key):
+        return self.session.get(Paths.PLAN_ATTACH.format(test_plan_key))
+
+    def create_attachments(self, test_plan_key, file_path):
+        return self.session.post_file(Paths.PLAN_ATTACH.format(test_plan_key),
+                                      file_path)
+
+    def search_plans(self, query, **params):
+        params.update({"query": query})
+        return self.session.get(Paths.PLAN_SEARCH, params=params)
+
+
+class TestRunEndpoints(EndpointTemplate):
+    """Api wrapper for "Test Run" endpoints"""
+
+    def create_test_run(self, project_key, name, **kwargs):
+        json = {"projectKey": project_key,
+                "name": name}
+        json.update(kwargs)
+        return self.session.post(Paths.RUN, json=json)
+
+    def get_test_run(self, test_run_key, **params):
+        return self.session.get(Paths.RUN_KEY.format(test_run_key),
+                                params=params)
+
+    def delete_test_run(self, test_run_key):
+        return self.session.delete(Paths.RUN_KEY.format(test_run_key))
+
+    def get_attachments(self, test_run_key):
+        return self.session.get(Paths.RUN_ATTACH.format(test_run_key))
+
+    def create_attachments(self, test_run_key, file_path):
+        return self.session.post_file(Paths.RUN_ATTACH.format(test_run_key),
+                                      file_path)
+
+    def create_test_result(self, test_run_key, test_case_key, **json):
+        return self.session.post(Paths.RUN_TEST_RESULT.format(test_run_key, test_case_key),
+                                 json=json)
+
+    def update_test_result(self, test_run_key, test_case_key, **json):
+        return self.session.post(Paths.RUN_TEST_RESULT.format(test_run_key, test_case_key),
+                                 json=json)
+
+    def get_test_results(self, test_run_key):
+        return self.session.get(Paths.RUN_TEST_RESULTS.format(test_run_key))
+
+    def create_test_results(self, test_run_key, **json):
+        return self.session.post(Paths.RUN_TEST_RESULTS.format(test_run_key),
+                                 json=json)
+
+    def search_runs(self, query, **params):
+        params.update({"query": query})
+        return self.session.get(Paths.PLAN_SEARCH, params=params)
