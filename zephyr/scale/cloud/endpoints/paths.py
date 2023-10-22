3b5ed4ccc43b031b93c2f349df90f0ea583f9861
Petr Sharapenko <nassauwinter@gmail.com>
2023-10-22 23:13:51 +0200

    Refactor Cloud API: Add Cloud API paths to paths file

--- zephyr/scale/cloud/endpoints/paths.py
+++ zephyr/scale/cloud/endpoints/paths.py
@@ -16,3 +16,61 @@ class CloudPaths:
     CASE_VER = "testcases/{}/versions/{}"
     CASE_SCRIPT = "testcases/{}/testscript"
     CASE_STEPS = "testcases/{}/teststeps"
+
+    # Test Cycles
+    CYCLES = "testcycles"
+    CYCLE_KEY = "testcycles/{}"
+    CYCLE_LINKS = "testcycles/{}/links"
+    CYCLE_ISSUES = "testcycles/{}/links/issues"
+    CYCLE_WEBLINKS = "testcycles/{}/links/weblinks"
+
+    # Test Plans
+    PLANS = "testplans"
+    PLAN_KEY = "testplans/{}"
+    PLAN_WEBLINKS = "testplans/{}/links/weblinks"
+    PLAN_ISSUES = "testplans/{}/links/issues"
+    PLAN_CYCLES = "testplans/{}/links/testcycles"
+
+    # Test Executions
+    EXECUTIONS = "testexecutions"
+    EXECUTIONS_KEY = "testexecutions/{}"
+    EXECUTIONS_LINKS = "testexecutions/{}/links"
+    EXECUTIONS_ISSUES = "testexecutions/{}/links/issues"
+
+    # Folders
+    FOLDERS = "folders"
+    FOLDER_KEY = "folders/{}"
+
+    # Statuses
+    STATUSES = "statuses"
+    STATUSES_ID = "statuses/{}"
+
+    # Priorities
+    PRIORITIES = "priorities"
+    PRIORITIES_ID = "priorities/{}"
+
+    # Environments
+    ENVIRONMENTS = "environments"
+    ENVIRONMENTS_ID = "environments/{}"
+
+    # Projects
+    PROJECTS = "projects"
+    PROJECTS_KEY = "projects/{}"
+
+    # Links
+    LINKS_ID = "links/{}"
+
+    # Issue Links
+    ISLINKS_CASES = "issuelinks/{}/testcases"
+    ISLINKS_CYCLES = "issuelinks/{}/testcycles"
+    ISLINKS_PLANS = "issuelinks/{}/testplans"
+    ISLINKS_EXECS = "issuelinks/{}/executions"
+
+    # Automations
+    AUT_CUSTOM = "automations/executions/custom"
+    AUT_CUCUMBER = "automations/executions/cucumber"
+    AUT_JUNIT = "automations/executions/junit"
+    AUT_CASES = "automations/testcases"
+
+    # Healthcheck
+    HEALTHCHECK = "healthcheck"
